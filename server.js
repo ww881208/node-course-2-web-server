92f01922015a2ddb9f5fd1543a2c05ae7dcff637
wei <wei@weis-MacBook-Air.local>
2018-04-04 23:13:53 -0500

    update codet

--- server.js
+++ server.js
@@ -7,9 +7,32 @@ var app = new express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
+//app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    var log = new Date().toString() + " : " + req.method + ' ' + req.url;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err)
+            console.log('Unable to write to server.log.');
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'Maintenance Page',
+//         message: 'The site is currently updating.'
+//     });
+// });
+
 app.use(express.static(__dirname + '/public'));
+
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
 });
 
 app.get('/', (req, res) => {
